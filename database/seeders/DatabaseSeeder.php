48600a9cddf8b91cfdfc7409e44d4c12d01eed7a
jampibryan <jampibryan@gmail.com>
2022-08-04 13:28:20 -0500

    Seeders_3 Tablas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // CLIENTES
+        $cliente = new Cliente();
+        
+        $cliente->dni = "76823980";
+        $cliente->nombres = "Bryan Amaya";
+        $cliente->celular = "935172287";
+        
+        $cliente->save();
 
+        // COMPRAS
+        
+        $compra = new Compra();
+        
+        $compra->dni = "76823980";
+        $compra->tipoMoneda = "euro";
+        $compra->montoComprar = "150";
+    
+        $compra->save();
+
+
+        // VENTAS
+
+        $venta = new Venta();
+        
+        $venta->dni = "76823980";
+        $venta->tipoMoneda = "euro";
+        $venta->montoVender = "80";
+    
+        $venta->save();
     }
 }
