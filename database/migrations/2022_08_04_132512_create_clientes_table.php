5c8bca4034d83a2167c9aa50eb8a5467a35cbcf2
jampibryan <jampibryan@gmail.com>
2022-08-04 08:54:53 -0500

    Instalación AdminLte

---
+++ database/migrations/2022_08_04_132512_create_clientes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('dni', 8);
+            $table->string('nombres', 30);
+            $table->string('celular',9);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
---
+++ resources/views/Clientes/index.blade.php
@@ -0,0 +1,66 @@
+@extends('adminlte::page')
+
+@section('title', 'Tipo de Cambio')
+
+@section('content_header')
+    <h1>CLIENTES REGISTRADOS</h1>
+@stop
+
+@section('content')
+
+    {{-- <h1 class=" flex justify-center text-4xl italic mb-8">TABLA DE REGISTROS DE CLIENTES</h1> --}}
+    <a class="w-16 bg-blue-400 py-2 px-4 italic text-2xl" href="{{ route('clientes.create') }}">Registrar
+        Cliente</a>
+
+    <div class="container flex justify-center mt-4">
+        <table class="bg-blue-400 border-8 border-double border-red-400">
+            <thead>
+                <tr>
+                    <th class="border text-center px-8 py-2 italic text-2xl">Id</th>
+                    <th class="border text-center px-8 py-2 italic text-2xl">DNI</th>
+                    <th class="border text-center px-8 py-2 italic text-2xl">NOMBRE</th>
+                    <th class="border text-center px-8 py-2 italic text-2xl">CELULAR</th>
+                    <th class="border text-center px-8 py-2 italic text-2xl">ACCIONES</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($clientes as $cliente)
+                    <tr>
+                        <td class="border text-center px-4 py-2 italic text-2xl text-white">{{ $cliente->id }}
+                        </td>
+                        <td class="border text-center px-4 py-2 italic text-2xl text-white">{{ $cliente->dni }}
+                        </td>
+                        <td class="border text-center px-4 py-2 italic text-2xl text-white">
+                            {{ $cliente->nombres }}</td>
+                        <td class="border text-center px-4 py-2 italic text-2xl text-white">
+                            {{ $cliente->celular }}</td>
+
+                        <td class="border text-center px-4 py-2 italic text-base">
+                            <a href="{{ route('clientes.edit', $cliente) }}" class="bg-red-400 p-2">Editar</a>
+                            <form action="{{ route('clientes.destroy', $cliente) }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <button class="bg-black p-2 text-white">Eliminar</button>
+                            </form>
+                        </td>
+
+                    </tr>
+                @endforeach
+            </tbody>
+
+        </table>
+    </div>
+
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script>
+        console.log('Hi!');
+    </script>
+@stop
